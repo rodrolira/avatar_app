9581271931e1d925ed9230213d59537f0bf5c30a
Rodrigo Lira <rlirapizarro@gmail.com>
2023-06-17 20:35:17 -0400

    Agregando controlador con acción index y captura de avatars. Además se modifica el seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,11 @@
-10.times do |i|
- Image.create(url_image:
-Faker::Avatar.image)
+avatars_data = 10.times.map do
+  {
+    url_image: Faker::Internet.url,
+  }
 end
+
+avatars = avatars_data.map { |data| Image.create!(data) }
+
+puts "#{avatars.count} avatars creados"
+
+AvatarsController.class_variable_set(:@@avatars, avatars)
